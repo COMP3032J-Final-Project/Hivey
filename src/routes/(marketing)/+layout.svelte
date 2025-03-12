01e5902408702196e62f730162e317660415da5a
meowking <mr.meowking@posteo.com>
2025-03-12 14:44:21 +0800

    refactor: group marketing related routes

---
+++ src/routes/(marketing)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Footer from './components/Footer.svelte';
+    let { children } = $props();
+</script>
+
+<div class="flex flex-col min-h-screen">
+    {@render children()}
+
+    <Footer />
+</div>
---
+++ src/routes/(marketing)/contact/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Header from '../components/Header.svelte';
+    let { children } = $props();
+</script>
+
+<Header />
+
+<main class="flex-grow">
+    {@render children()}
+</main>
