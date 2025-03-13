625c29d9b96dc342679eaa6142e26cda18480c2b
LiyanTao <641217688tly@gmail.com>
2025-03-13 21:03:55 +0800

    fix: push files fogot to commit

---
+++ src/lib/types/dashboard.d.ts
@@ -0,0 +1,6 @@
+import type { User } from './auth';
+
+// Dashboard布局数据接口
+export interface DashboardLayoutData {
+  user: User | null;
+} 
---
+++ src/routes/dashboard/+layout.ts
@@ -0,0 +1,36 @@
+import { browser } from '$app/environment';
+import { redirect } from '@sveltejs/kit';
+import { getUserInfo, getUserAuth } from '$lib/api/auth';
+import type { LayoutLoad } from './$types';
+import type { DashboardLayoutData } from '$lib/types/dashboard';
+import * as m from '$lib/paraglide/messages';
+import { failure } from '$lib/components/ui/toast';
+import type { User } from '$lib/types/auth';
+
+export const load: LayoutLoad = async (): Promise<DashboardLayoutData> => {
+	// 在服务器端渲染时返回null
+	if (!browser) {
+		return { user: null };
+	}
+
+	// 检查用户是否已登录
+	const userAuth = getUserAuth();
+	if (!userAuth) {
+		// 如果未登录，重定向到登录页面
+		failure(m.error_user_not_login());
+		redirect(302, '/auth/signin');
+	}
+
+	try {
+		// 获取用户信息
+		const user: User = await getUserInfo();
+
+		// TODO 获取用户的项目信息
+		// const projects: Project[] = await getUserProjects();
+		return { user };
+	} catch (error) {
+		console.error('获取用户信息失败:', error);
+		// 如果获取用户信息失败，返回null
+		return { user: null };
+	}
+};
