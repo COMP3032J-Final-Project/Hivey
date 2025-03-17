9313f55e79f9a1453f82de8e542fea27a9ea8693
chenjiawen <cnjywr@163.com>
2025-03-17 20:51:10 +0800

    feat: add new ui component - ToggleGroup

--- src/routes/edit/+page.svelte
+++ src/routes/edit/+page.svelte
@@ -6,13 +6,15 @@
     import * as AvatarGroup from '$lib/components/ui/avatar-group';
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
     import { Separator } from "$lib/components/ui/separator/index.js";
+    import * as ToggleGroup from "$lib/components/ui/toggle-group/index.js";
     import Editor from "$lib/components/editor.svelte";
     import Previewer from "$lib/components/previewer.svelte";
     import Exportbutton from "$lib/components/edit-button/export-button.svelte";
     import Watchbutton from "$lib/components/edit-button/watch-button.svelte";
     import FullScreenbutton from "$lib/components/edit-button/full-screen-button.svelte";
-    import BoldButton from '$lib/components/edit-button/bold-button.svelte';
-    import ItalicButton from '$lib/components/edit-button/italic-button.svelte';
+    import Bold from "@lucide/svelte/icons/bold";
+    import Italic from "@lucide/svelte/icons/italic";
+    import Underline from "@lucide/svelte/icons/underline";
     
     let docContent = $state("");
 
@@ -105,8 +107,17 @@
                     <Sidebar.Trigger class="-ml-1" />
                     <Separator orientation="vertical" class="mr-2 h-4" />
                     <Button variant="secondary" size="sm" onclick={formatMarkdown}>Markdown</Button>
-                    <BoldButton/>
-                    <ItalicButton/>
+                    <ToggleGroup.Root type="multiple">
+                        <ToggleGroup.Item value="bold" aria-label="Toggle bold">
+                          <Bold class="size-4" />
+                        </ToggleGroup.Item>
+                        <ToggleGroup.Item value="italic" aria-label="Toggle italic">
+                          <Italic class="size-4" />
+                        </ToggleGroup.Item>
+                        <ToggleGroup.Item value="strikethrough" aria-label="Toggle strikethrough">
+                          <Underline class="size-4" />
+                        </ToggleGroup.Item>
+                    </ToggleGroup.Root>
                 </div>
                 <div class="flex-1 h-full">
                     <Editor bind:value={docContent}/>
@@ -119,7 +130,7 @@
                 <div class="p-2 border-b">
                     <div class="flex justify-between items-center">
                         <div class="flex space-x-2">
-                            <p class="p-3 h-9 flex items-center justify-center">Preview</p>
+                            <p class="p-3 h-10 flex items-center justify-center">Preview</p>
                         </div>
                         <div class="flex space-x-2">
                             <Exportbutton/>
