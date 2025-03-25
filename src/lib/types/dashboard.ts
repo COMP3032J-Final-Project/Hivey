282c34df9b516b9cbe74d623966744453032e19f
meowking <mr.meowking@posteo.com>
2025-03-25 22:01:43 +0800

    fix: lib/typpes/dashobaord issue

---
+++ src/lib/types/dashboard.ts
@@ -0,0 +1,37 @@
+import { User } from './auth';
+import * as v from 'valibot';
+
+// /routes/dashboard/+layout.ts的load函数返回的数据类型
+export interface DashboardLayoutData {
+    user: User | null;
+}
+
+export enum ProjectType {
+    Project = 'project',
+    Template = 'template',
+}
+
+export enum ProjectFormCategory {
+    Blank = 'blank',
+    Upload = 'upload',
+    Example = 'example'
+}
+
+export const Project = v.object({
+    id: v.string(),
+    name: v.string(),
+    createdAt: v.date(),
+    updatedAt: v.date(),
+    type: v.enum(ProjectType),
+    owner: v.optional(User) ,
+})
+
+export type Project = v.InferOutput<typeof Project>;
+
+
+export const CreateProjectForm = v.object({
+    name: v.string(),
+    category: v.enum(ProjectType),
+    file: v.optional(v.instanceof(File))
+})
+export type CreateProjectForm = v.InferOutput<typeof CreateProjectForm>;
