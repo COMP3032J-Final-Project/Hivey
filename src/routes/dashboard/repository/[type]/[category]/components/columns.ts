d8c82868797634960187961fec240e2efee04330
Yiran Zhao <1911977514@qq.com>
2025-04-19 01:38:17 +0800

    fix(dashboard page): checkbox deletes projects

--- src/routes/dashboard/repository/[type]/[category]/components/columns.ts
+++ src/routes/dashboard/repository/[type]/[category]/components/columns.ts
@@ -13,20 +13,24 @@ export const getColumns = (type: string, currentUser: User | null): ColumnDef<Pr
 	const baseColumns: ColumnDef<Project>[] = [
 		{
 			id: 'select',
-			header: ({ table }) =>
-				renderComponent(Checkbox, {
+			header: ({ table }) => {
+				if (!currentUser) return null;
+				return renderComponent(Checkbox, {
 					checked: table.getIsAllPageRowsSelected(),
 					indeterminate:
 						table.getIsSomePageRowsSelected() && !table.getIsAllPageRowsSelected(),
 					onCheckedChange: (value: boolean) => table.toggleAllPageRowsSelected(!!value),
 					'aria-label': 'Select all'
-				}),
-			cell: ({ row }) =>
-				renderComponent(Checkbox, {
+				});
+			},
+			cell: ({ row }) => {
+				if (!currentUser || currentUser.email !== row.original.owner?.email) return null;
+				return renderComponent(Checkbox, {
 					checked: row.getIsSelected(),
 					onCheckedChange: (value: boolean) => row.toggleSelected(!!value),
 					'aria-label': 'Select row'
-				}),
+				});
+			},
 			enableSorting: false,
 			enableHiding: false
 		},
